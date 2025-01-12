06cf61a4dbcc89bbdc407135481146046d3d61ce
Ukas <siqxdpo@gmail.com>
2025-01-12 23:53:39 +0100

    chore: Added img  folder and made overall project adjustments

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/dashboard.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'primary': '#0075ff',
+        'primary-shade': '#0d69d5',
+        'orange-color': '#f59e0b',
+        'green-color': '#22c55e',
+        'red-color': '#f44336',
+        'grey-color': '#888',
+      },
+      fontFamily:{
+        roboto:['"Roboto"', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
