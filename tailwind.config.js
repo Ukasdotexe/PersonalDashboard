666c5987f0c9b59cd2f70db955ae246a2a2b4dd0
Ukas <siqxdpo@gmail.com>
2025-01-14 16:10:50 +0100

    Add courses page with initial layout and styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/dashboard.html"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
