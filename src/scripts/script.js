76844c838587f2e660ac4f656d6f132fca89fdd4
Ukas <siqxdpo@gmail.com>
2025-01-16 10:49:25 +0100

    Implement input focus handling with event delegation for icon color change and Refactoring attachEventListeners function to handle clicks for multiple elements via event delegation

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,3 +1,34 @@
+//
+//
+//
+
+const pathMap = {
+  courses: "courses.html",
+  files: "files.html",
+  plans: "plans.html",
+  friends: "friends.html",
+  projects: "projects.html",
+  // settings: "Settings.html",
+};
+
+const attachEventListeners = (containerId, mapping) => {
+  const containerEl = document.getElementById(containerId);
+  // console.log(containerEl);
+
+  containerEl.addEventListener("click", (event) => {
+    let target = event.target;
+
+    if (target.tagName != "a") target = target.closest("a");
+
+    if (mapping[target.id]) {
+      loadHtmlChild(mapping[target.id]);
+    }
+  });
+};
+
+// Initialize the event listeners by passing the container ID and the mapping
+attachEventListeners("sidenav", pathMap);
+
 const loadHtmlChild = async function (path) {
   try {
     //
@@ -47,10 +78,26 @@ const attackClickEvent = function (element, path) {
   element.addEventListener("click", () => loadHtmlChild(path));
 };
 
-attackClickEvent(coursesEl, "courses.html");
-attackClickEvent(filesEl, "files.html");
-attackClickEvent(plansEl, "plans.html");
-attackClickEvent(friendsEl, "friends.html");
-attackClickEvent(projectEl, "projects.html");
+// attackClickEvent(coursesEl, "courses.html");
+// attackClickEvent(filesEl, "files.html");
+// attackClickEvent(plansEl, "plans.html");
+// attackClickEvent(friendsEl, "friends.html");
+// attackClickEvent(projectEl, "projects.html");
 
 // loadHtmlChild("files.html");
+
+const formContainer = document.getElementById("social-media-username-form");
+
+formContainer.addEventListener("focusin", (event) => {
+  if (event.target.tagName === "INPUT") {
+    const icon = event.target.nextElementSibling.querySelector("i");
+    icon.classList.remove("text-grey-color");
+  }
+});
+
+formContainer.addEventListener("focusout", (event) => {
+  if (event.target.tagName === "INPUT") {
+    const icon = event.target.nextElementSibling.querySelector("i");
+    icon.classList.add("text-grey-color");
+  }
+});
